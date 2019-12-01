81576cc035026640e2c2cdbddd50b23b9dd4a944
bugra <bgraknd@gmail.com>
2019-12-01 18:28:02 +0300

    add signin

--- app/src/main/java/com/bugrakandemir/instaclonefb/MainActivity.kt
+++ app/src/main/java/com/bugrakandemir/instaclonefb/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.bugrakandemir.instaclonefb
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth : FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +22,25 @@ class MainActivity : AppCompatActivity() {
 
     fun signInClicked(view: View) {
 
-        auth.signInWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString()).addOnCompleteListener { task ->
+        auth.signInWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString())
+            .addOnCompleteListener { task ->
 
-            if (task.isSuccessful){
-                Toast.makeText(applicationContext,"Welcome: ${auth.currentUser?.email.toString()}",Toast.LENGTH_LONG).show()
-                val intent = Intent(applicationContext, FeedActivity::class.java)
-                startActivity(intent)
-                finish()
-            }
-        }.addOnFailureListener { exception ->
-            Toast.makeText(applicationContext, exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Welcome: ${auth.currentUser?.email.toString()}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    val intent = Intent(applicationContext, FeedActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }.addOnFailureListener { exception ->
+            Toast.makeText(
+                applicationContext,
+                exception.localizedMessage?.toString(),
+                Toast.LENGTH_LONG
+            ).show()
         }
 
     }
@@ -42,14 +51,18 @@ class MainActivity : AppCompatActivity() {
         val password = passwordText.text.toString()
 
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            if (task.isSuccessful){
+            if (task.isSuccessful) {
                 val intent = Intent(applicationContext, FeedActivity::class.java)
                 startActivity(intent)
                 finish()
             }
         }.addOnFailureListener { exception ->
-            if (exception != null){
-                Toast.makeText(applicationContext, exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
+            if (exception != null) {
+                Toast.makeText(
+                    applicationContext,
+                    exception.localizedMessage.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
 
